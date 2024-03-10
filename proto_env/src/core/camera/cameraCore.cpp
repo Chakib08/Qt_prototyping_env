640b855ea5601d474d73b40321df2f4f25bb1148
Mohamed Lamine Kartobi <chakibdace@gmail.com>
2024-03-11 00:41:49 +0100

    Some camera functions

--- proto_env/src/core/camera/cameraCore.cpp
+++ proto_env/src/core/camera/cameraCore.cpp
@@ -1,6 +1,28 @@
 #include "cameraCore.h"
 
+void cameraCore::initialize()
+{
+
+}
+
+void cameraCore::start()
+{
+    m_camera->start();
+    m_mediaCaptureSession->setCamera(m_camera.data());
+}
+
+void cameraCore::stop()
+{
+
+}
+
 cameraCore::cameraCore()
+{
+    m_camera = QSharedPointer<QCamera>(new QCamera(QMediaDevices::defaultVideoInput()));
+    m_mediaCaptureSession = QSharedPointer<QMediaCaptureSession>(new QMediaCaptureSession(this));
+}
+
+cameraCore::~cameraCore()
 {
 
 }
--- proto_env/src/core/camera/cameraCore.h
+++ proto_env/src/core/camera/cameraCore.h
@@ -1,15 +1,26 @@
 #ifndef CAMERACORE_H
 #define CAMERACORE_H
 
+#include <QCamera>
+#include <QImageCapture>
+#include <QMediaDevices>
+#include <QMediaCaptureSession>
+#include <QSharedPointer>
+
 #include "src/core/iDeviceCore.h"
 
 class cameraCore : public IDeviceCore
 {
+
+public:
+    cameraCore();
+    ~cameraCore();
     void initialize() override;
     void start() override;
     void stop() override;
-public:
-    cameraCore();
+private:
+    QSharedPointer<QCamera> m_camera;
+    QSharedPointer<QMediaCaptureSession> m_mediaCaptureSession;
 };
 
 #endif // CAMERACORE_H
